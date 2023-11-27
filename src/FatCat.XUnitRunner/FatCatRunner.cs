c6e12dda15aef12ab617997197a66f283b8a8022
David Basarab <dbasarab617@gmail.com>
2023-11-27 12:48:34 -0500

    moved to dotnet 7 and 8 and updated pacakges

--- src/FatCat.XUnitRunner/FatCatRunner.cs
+++ src/FatCat.XUnitRunner/FatCatRunner.cs
@@ -3,53 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
 namespace FatCat.XUnitRunner
 {
-	internal class FatCatRunner : XunitTestAssemblyRunner
+internal class FatCatRunner : XunitTestAssemblyRunner
+{
+	private readonly List<ITestFixture> testFixtures = new List<ITestFixture>();
+
+	public FatCatRunner(ITestAssembly testAssembly, IEnumerable<IXunitTestCase> testCases, IMessageSink diagnosticMessageSink, IMessageSink executionMessageSink, ITestFrameworkExecutionOptions executionOptions)
+		: base(testAssembly, testCases, diagnosticMessageSink, executionMessageSink, executionOptions) { }
+
+	protected override async Task AfterTestAssemblyStartingAsync()
 	{
-		private readonly List<ITestFixture> testFixtures = new List<ITestFixture>();
+		// Let everything initialize
+		await base.AfterTestAssemblyStartingAsync();
 
-		public FatCatRunner(ITestAssembly testAssembly, IEnumerable<IXunitTestCase> testCases, IMessageSink diagnosticMessageSink, IMessageSink executionMessageSink, ITestFrameworkExecutionOptions executionOptions)
-			: base(testAssembly, testCases, diagnosticMessageSink, executionMessageSink, executionOptions) { }
+		Aggregator.Run(() =>
+						{
+							var assembly = ((IReflectionAssemblyInfo)TestAssembly.Assembly).Assembly;
 
-		protected override async Task AfterTestAssemblyStartingAsync()
-		{
-			// Let everything initialize
-			await base.AfterTestAssemblyStartingAsync();
+							var testFixtureTypes = assembly.GetTypes().Where(i => i.GetInterfaces().Any(k => k.FullName == typeof(ITestFixture).FullName));
 
-			Aggregator.Run(() =>
+							foreach (var currentType in testFixtureTypes)
 							{
-								var assembly = ((IReflectionAssemblyInfo)TestAssembly.Assembly).Assembly;
+								var testFixture = (ITestFixture)Activator.CreateInstanceFrom(assembly.Location, currentType.FullName).Unwrap();
 
-								var testFixtureTypes = assembly.GetTypes().Where(i => i.GetInterfaces().Any(k => k.FullName == typeof(ITestFixture).FullName));
+								testFixture.SetUp();
 
-								foreach (var currentType in testFixtureTypes)
-								{
-									var testFixture = (ITestFixture)Activator.CreateInstanceFrom(assembly.Location, currentType.FullName).Unwrap();
-
-									testFixture.SetUp();
-
-									testFixtures.Add(testFixture);
-								}
-							});
-		}
-
-		protected override Task BeforeTestAssemblyFinishedAsync()
-		{
-			foreach (var testFixture in testFixtures) testFixture.TearDown();
-
-			return base.BeforeTestAssemblyFinishedAsync();
-		}
-
-		protected override Task<RunSummary> RunTestCollectionAsync(IMessageBus messageBus, ITestCollection testCollection, IEnumerable<IXunitTestCase> testCases, CancellationTokenSource cancellationTokenSource)
-		{
-			var runner = new XunitTestCollectionRunner(testCollection, testCases, DiagnosticMessageSink, messageBus, TestCaseOrderer, new ExceptionAggregator(Aggregator), cancellationTokenSource);
-
-			return runner.RunAsync();
-		}
+								testFixtures.Add(testFixture);
+							}
+						});
 	}
+
+	protected override Task BeforeTestAssemblyFinishedAsync()
+	{
+		foreach (var testFixture in testFixtures) testFixture.TearDown();
+
+		return base.BeforeTestAssemblyFinishedAsync();
+	}
+
+	protected override Task<RunSummary> RunTestCollectionAsync(IMessageBus messageBus, ITestCollection testCollection, IEnumerable<IXunitTestCase> testCases, CancellationTokenSource cancellationTokenSource)
+	{
+		var runner = new XunitTestCollectionRunner(testCollection, testCases, DiagnosticMessageSink, messageBus, TestCaseOrderer, new ExceptionAggregator(Aggregator), cancellationTokenSource);
+
+		return runner.RunAsync();
+	}
+}
 }
